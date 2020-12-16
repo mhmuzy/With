c1efe17c3d9100a55dbcbdab2a5cbe7a9232633c
marcio henrique <mhmuzy@embratel.com.br>
2020-12-16 13:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto.Entidades/Socios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Entidades
+{
+    public class Socios
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
